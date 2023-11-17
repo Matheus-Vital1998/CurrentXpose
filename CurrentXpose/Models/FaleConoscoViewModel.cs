658d7ddaac77293b40fc95dcda1cc9c46809fab4
Matheus Oliveira <fh185@t-systems.com.br>
2023-11-17 09:40:43 -0300

    [FEAT] - Implementando tela de fale conosco

---
+++ CurrentXpose/Models/FaleConoscoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CurrentXpose.Models
+{
+    public class FaleConoscoViewModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
